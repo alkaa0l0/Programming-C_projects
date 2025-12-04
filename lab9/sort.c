da2a98464e9299fd08955a6261b1f38c9edf6d42
alkaa0l0 <alinagumenyuk07@gmail.com>
2025-12-04 23:14:53 +0700

    Add file greeting.txt

---
+++ lab9/sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// Быстрая сортировка
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        int pi = i + 1;
+        
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int numbers[] = {64, 34, 25, 12, 22, 90};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    
+    quickSort(numbers, 0, size - 1);
+    
+    printf("Sorted array: ");
+    printf("hello git\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
